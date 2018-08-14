945b38aa3da42a0342983491811d05ded55039fd
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-14 22:08:52 +0500

    first commit from hostel

---
+++ Ch8-P7-CustomEnumeratorWithYield/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_P7_CustomEnumeratorWithYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
