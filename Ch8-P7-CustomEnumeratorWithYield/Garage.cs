27038798c45212a8d50200b817fab653993e7d5c
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-14 22:23:36 +0500

    commit 2 from hostel

---
+++ Ch8-P7-CustomEnumeratorWithYield/Garage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_P6_CustomEnumerator
+{
+    public class Garage : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+        // Fill with some Car objects upon startup.
+        public Garage()
+        {
+            carArray[0] = new Car("Cultus", 30);
+            carArray[1] = new Car("Corolla", 55);
+            carArray[2] = new Car("Civic", 30);
+            carArray[3] = new Car("Mira", 30);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return carArray.GetEnumerator();
+        }
+    }
+}
